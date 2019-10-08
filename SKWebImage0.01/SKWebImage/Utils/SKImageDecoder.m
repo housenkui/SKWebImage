82499f44f1a2a8ae2490dd666584ffc595073ca2
侯森魁 <housenkui@gmail.com>
2019-10-08 15:29:15 +0800

    Creating the CGBitmapContext with the right bytes per pixel and bitmap info depending on the original image. This removes an error that was making CGBitmapContext return NULL with some images: <Error>: CGBitmapContextCreate: unsupported parameter combination: 8 integer bits/component; 32 bits/pixel; 3-component color space; kCGImageAlphaLast; 400 bytes/row.

--- SKWebImage0.01/SKWebImage/Utils/SKImageDecoder.m
+++ SKWebImage0.01/SKWebImage/Utils/SKImageDecoder.m
@@ -80,20 +80,19 @@
     CGImageRef imageRef =  image.CGImage;
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     CGImageAlphaInfo alphaInfo = CGImageGetAlphaInfo(imageRef);
+    
+    BOOL imageHasAlphaInfo = (alphaInfo != kCGImageAlphaNone);
+    int bytesPerPixel = imageHasAlphaInfo ? 4 : 3;
+    CGBitmapInfo bitmapInfo = imageHasAlphaInfo ? kCGImageAlphaPremultipliedLast:kCGImageAlphaNone;
     CGContextRef context = CGBitmapContextCreate(NULL,
                                                  CGImageGetWidth(imageRef),
                                                  CGImageGetHeight(imageRef),
                                                  8,
-                                                 // Just always return width * 4 will be enough
-                                                 CGImageGetWidth(imageRef) * 4,
+                                                 // Just always return width * bytesPerPixel will be enough
+                                                 CGImageGetWidth(imageRef) * bytesPerPixel,
                                                  //System only supports RGB,set explicitly
                                                  colorSpace,
-                                                 //Make system don't need to do extra conversion when displayed.
-                                                 // NOTE: here we remove the alpha channel for performance. Most of the time, images loaded
-                                                 //       from the network are jpeg with no alpha channel. As a TODO, finding a way to detect
-                                                 //       if alpha channel is necessary would be nice.
-
-                                                 alphaInfo | kCGBitmapByteOrder32Little);
+                                                 bitmapInfo);
     CGColorSpaceRelease(colorSpace);
     if (!context) {
         return nil;
